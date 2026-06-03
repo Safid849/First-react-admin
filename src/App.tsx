3754d0bfdf67b41b7428fb1d853b98055dd0fa86
Zinedis <nantenainabakari@gmail.com>
2026-06-03 09:19:54 +0300

    fix: resolve version json-serve

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
-import { Admin } from "react-admin";
-import { Layout } from "./Layout";
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { EmployeeList } from './employees/EmployeeList';
+import { EmployeeCreate } from './employees/EmployeeCreate';
+import { EmployeeEdit } from './employees/EmployeeEdit';
+import { EmployeeShow } from './employees/EmployeeShow';
 
-export const App = () => <Admin layout={Layout}></Admin>;
+const dataProvider = jsonServerProvider('http://localhost:3002');
+
+export const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource 
+            name="employees" 
+            list={EmployeeList} 
+            create={EmployeeCreate} 
+            edit={EmployeeEdit} 
+            show={EmployeeShow} 
+        />
+    </Admin>
+);
